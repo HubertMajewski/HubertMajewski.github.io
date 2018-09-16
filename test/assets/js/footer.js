56cd34055cff5522ba4832c9cce1fc627dada07f
Hubert <Hubert@Acer>
2018-09-16 08:13:33 -0400

    Init

---
+++ test/assets/js/footer.js
@@ -0,0 +1,23 @@
+document.write('\
+			<!-- Footer -->\
+			<footer id="footer">\
+\
+				<ul class="icons">\
+					<li><a href="#" class="icon circle fa-twitter"><span class="label">Twitter</span></a></li>\
+					<!--<li><a href="#" class="icon circle fa-facebook"><span class="label">Facebook</span></a></li>\
+					<li><a href="#" class="icon circle fa-google-plus"><span class="label">Google+</span></a></li>-->\
+					<li><a href="https://HubertMajewski.github.io" class="icon circle fa-github"><span class="label">Github</span></a></li>\
+					<!--<li><a href="#" class="icon circle fa-dribbble"><span class="label">Dribbble</span></a></li>-->\
+				</ul>\
+\
+				<ul class="copyright">\
+					<!--<li>&copy; Untitled</li>--><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>\
+				</ul>\
+\
+				<ul class="copyright" style="font-size: 24px;">\
+					<li><p>As our circle of knowledge expands, so does the circumference of darkness surrounding it. - Albert Einstein</p></li>\
+				</ul>\
+\
+			</footer>\
+');
+
