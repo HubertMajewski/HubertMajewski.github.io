b7efa11fca80fdb4e2d60720022fe8bf7b1b3abe
Hubert <Hubert@Acer>
2018-09-23 21:51:37 -0400

    Minor changes

--- test/assets/js/footer.js
+++ test/assets/js/footer.js
@@ -6,7 +6,7 @@ document.write('\
 					<li><a href="https://twitter.com/Hubert_Majewski" class="icon circle fa-twitter"><span class="label">Twitter</span></a></li>\
 					<!--<li><a href="#" class="icon circle fa-facebook"><span class="label">Facebook</span></a></li>\
 					<li><a href="#" class="icon circle fa-google-plus"><span class="label">Google+</span></a></li>-->\
-					<li><a href="https://HubertMajewski.github.io" class="icon circle fa-github"><span class="label">Github</span></a></li>\
+					<li><a href="https://github.com/HubertMajewski" class="icon circle fa-github"><span class="label">Github</span></a></li>\
 					<!--<li><a href="#" class="icon circle fa-dribbble"><span class="label">Dribbble</span></a></li>-->\
 				</ul>\
 \
