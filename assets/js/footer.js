8e900e68beef278458a2786a5c6c1c6ea85149fc
Hubert Majewski <36614010+HubertMajewski@users.noreply.github.com>
2022-04-21 14:41:46 -0400

    Update footer.js

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -4,7 +4,7 @@ document.write('\
 \
 				<ul class="icons">\
 					<li><a href="https://www.linkedin.com/in/hubert-m-4235b8143" class="icon circle fa-linkedin-square"><span class="label">LinkedIn</span></a></li>\
-					<li><a href="https://twitter.com/Hubert_Majewski" class="icon circle fa-twitter"><span class="label">Twitter</span></a></li>\
+					<!--<li><a href="https://twitter.com/" class="icon circle fa-twitter"><span class="label">Twitter</span></a></li>-->\
 					<!--<li><a href="#" class="icon circle fa-facebook"><span class="label">Facebook</span></a></li>\
 					<li><a href="#" class="icon circle fa-google-plus"><span class="label">Google+</span></a></li>-->\
 					<li><a href="https://github.com/HubertMajewski" class="icon circle fa-github"><span class="label">Github</span></a></li>\
